356d12282aec042242b0cdb834d23ccff3493f5d
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-18 11:58:07 -0300

    Adicionei uma classe para ler e transformar as tabelas em graficos

--- src/app.py
+++ src/app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# import testeGrafico
+import TableToGraph
+
+# procedimentos = TableToGraph.Table("Procedimentos SJC", "ano", "quantidade", "./tables/Procedimentos/Sao Jose dos Campos.csv", 4, 13)
+# procedimentos.SaveFig("./static/img/", "grafico procedimentos sjc.svg")
+
 @app.route("/")
 def index():
     return render_template("index.html")
