e11b9d85033c6eb5b8c3a07126083d1201250614
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-20 16:33:57 -0300

    Simplifiquei a classe Table para uma função

--- src/TableToGraph.py
+++ src/TableToGraph.py
@@ -1,50 +1,42 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import io
+def TableToGraph(csvDir, title, bars, lineRange, xLabel, yLabel, figDir):
+    dataFrame = pd.read_csv(csvDir)
 
-class Table :
-    def __init__(self,
-        title,
-        xLabel, yLabel,
-        tableDir,
-        columns,
-        lineStep,
-        width,
-        colors = [
-            '#fa0606',
-            '#f8047e',
-            '#fe05ff',
-            '#870bf1',
-            '#0003fe',
-            '#0884f9',
-            '#02faf9',
-            '#03ff83',
-            '#06fb04',
-            '#7dff05',
-            '#fcfb09',
-            '#f98203']
-        ):
-        dataFrame = pd.read_csv(tableDir)
+    left = range(1, bars+1)
 
-        height = []
-        labels = []
+    height = []
+    labels = []
 
-        j = 0
-        for i in range(0, len(dataFrame.values), lineStep):
-            if j > columns:
-                break
-            j += 1
+    j = 0
+    for i in lineRange:
+        j += 1
+        if j > bars:
+            break
+        labels.append(dataFrame.values[i][0])
+        height.append(dataFrame.values[i][-1])
 
-            labels.append(dataFrame.values[i][0])
-            height.append(dataFrame.values[i][-1])
-        
-        plt.bar(range(1, columns+1), height, tick_label = labels, width = width, color = colors)
+    colors = [
+        '#fa0606',
+        '#f8047e',
+        '#fe05ff',
+        '#870bf1',
+        '#0003fe',
+        '#0884f9',
+        '#02faf9',
+        '#03ff83',
+        '#06fb04',
+        '#7dff05',
+        '#fcfb09',
+        '#f98203']
+    plt.bar(left, height, tick_label = labels, width = 0.75, color = colors)
 
-        plt.xlabel(xLabel)
-        plt.ylabel(yLabel)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
 
-        plt.title(title)
-    
-    def SaveFig(self, path, name):
-        plt.savefig(path+name) #'./static/img/graficoTeste.svg')
+    plt.title(title)
+
+    plt.gcf().set_size_inches(10, 5)
+    plt.tight_layout()
+    plt.savefig(figDir)
