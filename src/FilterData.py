50c305c13fcb5e6972e3f38d61907c27cc03075b
Gabriel Lucena do Rosário <lucena15gr@gmail.com>
2023-05-26 18:34:07 -0300

    Upload com as tabelas finais de internações por morbidade em CID 10. Ativação dos filtros necessários para realizar a pesquisa corretamente.

--- src/FilterData.py
+++ src/FilterData.py
@@ -39,21 +39,21 @@ subTipos = [
         # { "name": "internacoes", "value": "CID II - Neoplasias da pele" },
         # { "name": "internacoes", "value": "CID II - Neoplasias e doenças do fígado" },
 
-        # { "name": "internacoes", "value": "CID III - Anemias por deficiência de ferro e outras anemias" },
-        # { "name": "internacoes", "value": "CID III - Afecções hemorrágicas e outras doenças do sangue e dos orgãos hematopoiéticos" },
+        { "name": "internacoes", "value": "CID III - Anemias por deficiência de ferro e outras anemias" },
+        { "name": "internacoes", "value": "CID III - Afecções hemorrágicas e outras doenças do sangue e dos orgãos hematopoiéticos" },
         # { "name": "internacoes", "value": "CID III - Transtornos envolvendo mecanismo imunitário" },
 
-        # { "name": "internacoes", "value": "CID IV - Diabetes mellitus" },
-        # { "name": "internacoes", "value": "CID IV - Transtornos nutricionais metabólicos e sequelas nutricionais" },
+        { "name": "internacoes", "value": "CID IV - Diabetes mellitus" },
+        { "name": "internacoes", "value": "CID IV - Transtornos nutricionais metabólicos e sequelas nutricionais" },
         # { "name": "internacoes", "value": "CID IV - Obesidade" },
 
         { "name": "internacoes", "value": "CID VI - Meningítes" },
         { "name": "internacoes", "value": "CID VI - Doenças de parkinson, alzheimer e epilepsia" },
         { "name": "internacoes", "value": "CID VI - Doenças Relacionadas ao sistema Nervoso" },
 
-        # { "name": "internacoes", "value": "CID IX - Infarto agúdo do miocárdio" },
-        # { "name": "internacoes", "value": "CID IX - Infarto cerebral" },
-        # { "name": "internacoes", "value": "CID IX - Insuficiência cardíaca" },
+        { "name": "internacoes", "value": "CID IX - Infarto agúdo do miocárdio" },
+        { "name": "internacoes", "value": "CID IX - Infarto cerebral" },
+        { "name": "internacoes", "value": "CID IX - Insuficiência cardíaca" },
 
         { "name": "internacoes", "value": "CID X - Pneumonia" },
         { "name": "internacoes", "value": "CID X - Bronquite e bronqueolíte agúda, doenças pulmonares crônicas e bronquequiectasia" },
