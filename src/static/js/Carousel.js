96b81e104c0382206839f09e0e043bc0928cd12c
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-05-29 15:07:37 -0300

    Alterei os botões do carousel para mostrar os anos

--- src/static/js/Carousel.js
+++ src/static/js/Carousel.js
@@ -5,36 +5,31 @@ var periodos = [
     "2022"
 ];
 
+var current = 0;
+
 window.addEventListener("load", function(){
-    carousels = document.getElementsByClassName("carousel");
+    let graficos = document.getElementById("carousel-graficos");
 
-    for(let i = 0; i < carousels.length; i++){
-        for(let j = 1; j < carousels[i].children.length - 1; j++)
-            carousels[i].children[j].style.display = "none";
-        carousels[i].children[1].style.display = "inline-block";
+    graficos.children[0].style.display = "inline-block";
+    for(let i = 1; i < graficos.children.length; i++)
+        graficos.children[i].style.display = "none";
 
-        carousels[i].children[0].innerText = "<1/"+(carousels[i].children.length-2).toString();
-        carousels[i].children[carousels[i].children.length-1].innerText = "1/"+(carousels[i].children.length-2).toString()+">";
-    }
+    document.getElementById("carousel-button-r").children[0].innerText = periodos[current+1];
+    document.getElementById("carousel-button-l").children[0].innerText = periodos[graficos.children.length-1];
 });
 
 function RollCarousel(carousel, roll){
-    let current = 1;
-    for(let i = 1; i < carousel.children.length - 1; i++){
-        if(carousel.children[i].style.display == "inline-block"){
-            current = i;
-            carousel.children[current].style.display = "none";
-        }
-    }
+    let graficos = document.getElementById("carousel-graficos");
+    graficos.children[current].style.display = "none";
 
     current += roll;
-    if(current >= carousel.children.length - 1)
-        current = 1;
-    else if(current < 1)
-        current = carousel.children.length - 2;
+    if(current < 0)
+        current = graficos.children.length-1;
+    else if (current >= graficos.children.length)
+        current = 0;
 
-    carousel.children[current].style.display = "inline-block";
+    graficos.children[current].style.display = "inline-block";
 
-    carousel.children[0].innerText = "<"+current.toString()+"/"+(carousel.children.length-2).toString();
-    carousel.children[carousel.children.length-1].innerText = current.toString()+"/"+(carousel.children.length-2).toString()+">";
+    document.getElementById("carousel-button-r").children[0].innerText = periodos[(current+1 >= graficos.children.length ? 0 : current+1)];
+    document.getElementById("carousel-button-l").children[0].innerText = periodos[(current-1 < 0 ? graficos.children.length-1 : current-1)];
 }
