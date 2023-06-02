9c325347d3d30eb7c0b6a7fc6bd8f1cda44f8d2d
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-06-02 15:01:21 -0300

    Implementei a criação dos graficos do bd

--- src/TableToGraph.py
+++ src/TableToGraph.py
@@ -17,36 +17,28 @@ colors = [
     '#fcfb09',
     '#f98203']
 
-# def BarGraph(csvDir, title, barCount, lineRange, xLabel, yLabel, figDir):
-#     dataFrame = ReadCSV(csvDir)
+def BarGraphFromDB(select, title, xLabel, yLabel, figDir):
+    dataFrame = pandas.DataFrame(select)
 
-#     left = range(0, barCount)
+    lines = []
+    bars = []
+    for val in dataFrame.values:
+        lines.append(val[0])
+        bars.append(val[1])
 
-#     bars = []
-#     lines = []
+    fig = pyplot.figure(figsize = (10, 5))
+    
+    pyplot.bar(lines, bars, 0.75, 0.0)
 
-#     j = 0
-#     for i in lineRange:
-#         j += 1
-#         if j > barCount:
-#             break
-#         lines.append(dataFrame.values[i][0][:3])
-#         for l in range(0, len(dataFrame.values[i])):
-#             print(dataFrame.values[i][l])
-#         bars.append(dataFrame.values[i][-1])
+    pyplot.xlabel(xLabel)
+    pyplot.ylabel(yLabel)
+    pyplot.title(title)
 
-#     pyplot.bar(left, bars, tick_label = lines, width = 0.75, color = colors)
+    MakeFolder()
 
-#     pyplot.xlabel(xLabel)
-#     pyplot.ylabel(yLabel)
-
-#     pyplot.title(title)
-
-#     MakeFolder()
-
-#     pyplot.gcf().set_size_inches(10, 5)
-#     pyplot.tight_layout()
-#     pyplot.savefig(figDir)
+    pyplot.gcf().set_size_inches(10, 5)
+    pyplot.tight_layout()
+    pyplot.savefig(figDir)
 
 def GroupedBarGraph(csvDir, title, barRange, lineRange, xLabel, yLabel, figDir, autoDivide = False, divisor = 1):
     dataFrame = ReadCSV(csvDir)
@@ -108,55 +100,6 @@ def GroupedBarGraph(csvDir, title, barRange, lineRange, xLabel, yLabel, figDir, 
     pyplot.gcf().set_size_inches(10, 5)
     pyplot.tight_layout()
     pyplot.savefig(figDir)
-
-# def PercentageGraph(csvDir, title, barRange, lineRange, xLabel, yLabel, figDir, autoDivide = False, divisor = 1):
-#     dataFrame = ReadCSV(csvDir)
-
-#     lines = []
-#     for i in lineRange:
-#         lines.append(dataFrame.values[i][0][:3])
-
-#     bars = {}
-#     for i in barRange:
-#         values = []
-#         for j in lineRange:
-#             value = 0
-#             try:
-#                 value = float(dataFrame.values[j][int(i)+1])
-#             except:
-#                 value = 0
-#             values.append(value / divisor)
-#         bars.update({ dataFrame.columns[int(i)+1]: numpy.array(values) })
-
-#     x = numpy.arange(len(lines))
-#     width = 1/(len(bars) + 1)
-#     multiplier = 0
-
-#     fig, axes = pyplot.subplots(layout='constrained')
-
-#     maxVal = 0
-#     for names, values in bars.items():
-#         if max(values) > maxVal:
-#             maxVal = max(values)
-
-#     for names, values in bars.items():
-#         offset = width * multiplier
-#         rects = axes.bar(x + offset, [int((x / maxVal) * 100) for x in values], width, label=names)
-#         axes.bar_label(rects)
-#         multiplier += 1
-
-#     axes.set_xlabel(xLabel)
-#     axes.set_ylabel((f"{yLabel} / {divisor}" if divisor > 1 else yLabel))
-#     axes.set_title(title)
-#     axes.set_xticks(x + width / len(bars), lines)
-#     axes.legend(loc='upper left')
-#     axes.set_ymargin(0.2)
-
-#     MakeFolder()
-
-#     pyplot.gcf().set_size_inches(10, 5)
-#     pyplot.tight_layout()
-#     pyplot.savefig(figDir)
 
 def PercentageLineGraph(csvDir, title, barRange, lineRange, xLabel, yLabel, figDir):
     dataFrame = ReadCSV(csvDir)
